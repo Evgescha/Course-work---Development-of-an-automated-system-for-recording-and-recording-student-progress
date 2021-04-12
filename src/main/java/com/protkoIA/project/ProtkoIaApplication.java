392cabb51b2979f71c96ffeb3a7c3095372ad37f
yauhescha <evgescha.maslov007@yandex.ru>
2021-04-12 11:14:51 +0300

    Ground of Application

---
+++ src/main/java/com/protkoIA/project/ProtkoIaApplication.java
@@ -0,0 +1,14 @@
+package com.protkoIA.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtkoIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtkoIaApplication.class, args);
+		System.out.println("Application started!");
+	}
+
+}
