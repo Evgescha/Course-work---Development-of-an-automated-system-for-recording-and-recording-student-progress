15ecab7a7bef3ad5186b7a68e952e8aee1bfdbc3
yauhescha <evgescha.maslov007@yandex.ru>
2021-04-12 15:35:09 +0300

    index page added

---
+++ src/main/java/com/protkoIA/project/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.protkoIA.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = { "", "/" })
+public class IndexController {
+
+	@GetMapping
+	String getindex() {
+		return "index";
+	}
+
+}
